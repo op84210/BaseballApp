c200db62dbfdeb0470adf07726445a4b26aca17b
kwlin <kwlin@isnis.com.tw>
2025-12-09 09:25:07 +0800

    Add migration for TeamGameStats and TeamSeasonRankingCache tables

--- Migrations/20251209012442_AddTeamGameStatsAndTeamSeasonRankingCache.Designer.cs
+++ Migrations/20251209012442_AddTeamGameStatsAndTeamSeasonRankingCache.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BaseballApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BaseballApp.Migrations
 {
     [DbContext(typeof(BaseballDbContext))]
-    partial class BaseballDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251209012442_AddTeamGameStatsAndTeamSeasonRankingCache")]
+    partial class AddTeamGameStatsAndTeamSeasonRankingCache
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.10");
